0d1199da5592de49829a393cc600277f842ffb49
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-11-06 20:59:10 +0900

    making toDoList btn
    버튼기능만 활성화
    input에 작성한 텍스트가 리스트에 추가되는 기능은 아직 미구현.

---
+++ app.js
@@ -0,0 +1,41 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+
+// Functions
+function addTodo(event){
+  // prevent form from submitting
+  event.preventDefault();
+
+  // todo div 할일+버튼1+버튼2 를 포함하는 DIV
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  // create li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hey';
+  newTodo.classList.add('todo-item'); // use this to CSS
+  todoDiv.appendChild(newTodo); // todoDiv 에 추가하기
+
+  // check  mark button
+  const completeButton = document.createElement('button');
+  completeButton.innerHTML ='<i class="fas fa-check"></i>';
+  completeButton.classList.add('complete-btn');
+  todoDiv.appendChild(completeButton);
+
+  // check  trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML ='<i class="fas fa-trash"></i>';
+  trashButton.classList.add('complete-btn');
+  todoDiv.appendChild(trashButton);
+
+  // append toDo List 
+  // 위에만든 기능들을 포함한 div를 
+  // 쿼리셀렉터로 선태한 todoList에 추가하기
+  todoList.appendChild(todoDiv); 
+}
